1b3a465fc918cfb7d9fe6e20a9f87c3454c33e6c
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2021-12-14 23:40:32 +0100

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,16 +25,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -62,6 +45,7 @@ abstract class ListItem {
 
 /// A ListItem that contains data to display a heading.
 class HeadingItem implements ListItem {
+
   final String heading;
 
   HeadingItem(this.heading);
@@ -98,6 +82,7 @@ class MessageItem implements ListItem {
 class _MyHomePageState extends State<MyHomePage> {
 
   Auth auth = Auth();
+
   final items = List<ListItem>.generate(
     1000,
       (i) => i % 6 == 0
@@ -129,35 +114,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add_alert),
-              tooltip: 'Show Snackbar',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('This is a snackbar')));
-              },
-            ),
-          ]),
+          actions: []),
       body: ListView.builder(
-        // Let the ListView know how many items it needs to build.
         itemCount: items.length,
-        // Provide a builder function. This is where the magic happens.
-        // Convert each item into a widget based on the type of item it is.
         itemBuilder: (context, index) {
           final item = items[index];
-
           return ListTile(
             title: item.buildTitle(context),
             subtitle: item.buildSubtitle(context),
